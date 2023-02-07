b3620f87fd575ba82a690583b43ff13d5450739d
Kailen James <sevenflags57@gmail.com>
2023-02-06 17:27:59 -0800

    misc stuff

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+
+const getSequelize = ()=> {
+	if(process.env.JAWSDB){
+		return new Sequelize(process.env.JAWSDB);
+	}
+	return new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASS,
+		{
+			host: process.env.DB_HOST,
+			dialect: "mysql",
+			port: 3306
+		}
+	);
+};
+
+const sequelize = getSequelize();
+
+module.exports = sequelize;
