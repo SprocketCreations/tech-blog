2741edb0110f7b14e6652ffb778a46909efe31de
Kailen James <sevenflags57@gmail.com>
2023-02-08 14:54:00 -0800

    Added session

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const sequelize = require("./config/connection");
 const db = require("./models");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const handlebars = expressHandlebars.create({});
+
+app.use(session({
+	secret: process.env.SESS_SECRET,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+	resave:false,
+	saveUninitialized:false,
+	store: new SequelizeStore({db: sequelize}),
+})); 
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
